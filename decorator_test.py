46b0ba55a5610976993081e0abf5ef0bfdfa4329
ganesh-125 <87231655+ganesh-125@users.noreply.github.com>
2021-09-26 17:53:04 +0530

    Update decorator_test.py

--- decorator_test.py
+++ decorator_test.py
@@ -1,1 +1,21 @@
 
+
+# Defining Decorator
+def string_lower(func):
+    def wrapper():
+        f = func()
+        return f.lower()
+    return wrapper
+def make_list(func):
+    def wrapper():
+        f = func()
+        return list(f)
+    return wrapper
+
+
+@make_list
+@string_lower
+def myprint():
+    return "GANESH"
+a = myprint()
+print(a)
